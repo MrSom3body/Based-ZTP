c6972100b5a6b45943fcdeb3beea0296edaf041f
Karun Sandhu <karun@sndh.dev>
2026-01-15 10:28:29 +0100

    nixos: add boot loader configuration

---
+++ modules/nixos/boot.nix
@@ -0,0 +1,16 @@
+{
+  flake.modules.nixos.nixos =
+    { pkgs, ... }:
+    {
+      boot = {
+        kernelPackages = pkgs.linuxPackages_latest;
+        loader = {
+          systemd-boot = {
+            enable = true;
+            configurationLimit = 10;
+          };
+          efi.canTouchEfiVariables = true;
+        };
+      };
+    };
+}
