4dcf30c2bfb339755ab833913212455e6d2021ab
Karun Sandhu <karun@sndh.dev>
2026-02-19 17:33:32 +0100

    flake.lock: update

--- modules/hosts/omega/default.nix
+++ modules/hosts/omega/default.nix
@@ -2,6 +2,10 @@
 {
   flake.modules.nixos."hosts/omega" = {
     networking = {
+      defaultGateway = {
+        interface = "enp1s0";
+        address = "10.0.1.128";
+      };
       interfaces = {
         "enp1s0".ipv4.addresses = lib.singleton {
           address = "10.0.1.254";
