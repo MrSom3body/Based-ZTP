1c601bbbd9ee80163ba618ce8b6fb845e070c3b9
Karun Sandhu <karun@sndh.dev>
2026-02-19 17:22:35 +0100

    networking: init DNS

---
+++ modules/nixos/networking.nix
@@ -0,0 +1,29 @@
+{
+  flake.modules.nixos.nixos = {
+    networking = {
+      useNetworkd = true;
+      networkmanager = {
+        enable = true;
+        dns = "systemd-resolved";
+      };
+
+      nameservers = [
+        "9.9.9.9#dns.quad9.net"
+        "149.112.112.112#dns.quad9.net"
+        "2620:fe::fe#dns.quad9.net"
+        "2620:fe::9#dns.quad9.net"
+      ];
+    };
+
+    services = {
+      # DNS resolver
+      resolved = {
+        enable = true;
+        settings.Resolve = {
+          DNSOverTLS = "true";
+          DNSSEC = "true";
+        };
+      };
+    };
+  };
+}
