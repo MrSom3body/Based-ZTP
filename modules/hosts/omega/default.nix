d449b4ca8c1a65e88565f38d1b0fa318fbc6ee9b
Karun Sandhu <karun@sndh.dev>
2026-02-24 16:05:56 +0100

    omega: disable dhcp for interfaces

--- modules/hosts/omega/default.nix
+++ modules/hosts/omega/default.nix
@@ -7,13 +7,19 @@
         address = "10.0.1.2";
       };
       interfaces = {
-        "ens33".ipv4.addresses = lib.singleton {
-          address = "10.0.1.254";
-          prefixLength = 24;
+        "ens33" = {
+          useDHCP = false;
+          ipv4.addresses = lib.singleton {
+            address = "10.0.1.254";
+            prefixLength = 24;
+          };
         };
-        "ens37".ipv4.addresses = lib.singleton {
-          address = "10.0.2.254";
-          prefixLength = 24;
+        "ens37" = {
+          useDHCP = false;
+          ipv4.addresses = lib.singleton {
+            address = "10.0.2.254";
+            prefixLength = 24;
+          };
         };
       };
     };
