723f0b4ff9907c946885975369c40e4f1801a10c
Karun Sandhu <karun@sndh.dev>
2026-02-24 16:58:47 +0100

    omega: add dhcp interface and enable nat

--- modules/hosts/omega/default.nix
+++ modules/hosts/omega/default.nix
@@ -2,25 +2,29 @@
 {
   flake.modules.nixos."hosts/omega" = {
     networking = {
-      defaultGateway = {
-        interface = "ens33";
-        address = "10.0.1.2";
-      };
       interfaces = {
-        "ens33" = {
+        "ens33".useDHCP = true;
+        "ens36" = {
           useDHCP = false;
           ipv4.addresses = lib.singleton {
             address = "10.0.1.254";
             prefixLength = 24;
           };
         };
-        "ens36" = {
+        "ens37" = {
           useDHCP = false;
           ipv4.addresses = lib.singleton {
             address = "10.0.2.254";
             prefixLength = 24;
           };
         };
+      };
+      nat = {
+        internalInterfaces = [
+          "ens36"
+          "ens37"
+        ];
+        externalInterface = "ens33";
       };
     };
   };
