4a706e25715f368f9fa5c2d8197ce7d02015aaec
Karun Sandhu <karun@sndh.dev>
2026-02-24 16:17:33 +0100

    {lambda,sigma}: add network configuration

--- modules/hosts/lambda/default.nix
+++ modules/hosts/lambda/default.nix
@@ -1,3 +1,17 @@
+{ lib, ... }:
 {
-  flake.modules.nixos."hosts/lambda" = { };
+  flake.modules.nixos."hosts/lambda" = {
+    networking = {
+      defaultGateway = {
+        interface = "ens33";
+        address = "10.0.2.254";
+      };
+      interfaces = {
+        "ens33".ipv4.addresses = lib.singleton {
+          address = "10.0.2.1";
+          prefixLength = 24;
+        };
+      };
+    };
+  };
 }
--- modules/hosts/sigma/default.nix
+++ modules/hosts/sigma/default.nix
@@ -1,3 +1,17 @@
+{ lib, ... }:
 {
-  flake.modules.nixos."hosts/sigma" = { };
+  flake.modules.nixos."hosts/sigma" = {
+    networking = {
+      defaultGateway = {
+        interface = "ens33";
+        address = "10.0.1.254";
+      };
+      interfaces = {
+        "ens33".ipv4.addresses = lib.singleton {
+          address = "10.0.1.1";
+          prefixLength = 24;
+        };
+      };
+    };
+  };
 }
