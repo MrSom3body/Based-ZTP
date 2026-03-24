cbaaf053d004ac3b988111b8dca26dc1338a3e60
Karun Sandhu <karun@sndh.dev>
2026-03-24 16:52:01 +0100

    hosts/all-mgmt: add dhcp server

--- modules/hosts/all-mgmt/default.nix
+++ modules/hosts/all-mgmt/default.nix
@@ -16,5 +16,32 @@
         };
       };
     };
+
+    services.kea.dhcp4 = {
+      enable = true;
+      settings = {
+        interfaces-config.interfaces = [ "ens33" ];
+        lease-database = {
+          type = "memfile";
+          persist = true;
+          name = "/var/lib/kea/dhcp4.leases";
+        };
+        subnet4 = lib.singleton {
+          subnet = "10.1.0.0/24";
+          pools = lib.singleton { pool = "10.1.0.1 - 10.1.0.99"; };
+          option-data = [
+            {
+              name = "routers";
+              data = "10.1.0.254";
+            }
+            {
+              name = "domain-name-servers";
+              data = "9.9.9.9, 149.112.112.112";
+            }
+          ];
+          valid-lifetime = 86400;
+        };
+      };
+    };
   };
 }
