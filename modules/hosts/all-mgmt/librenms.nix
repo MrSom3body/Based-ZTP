81ff59596fa533caa5a2b35d9e848541bb926213
Karun Sandhu <karun@sndh.dev>
2026-04-13 23:16:09 +0200

    librenms: add php to packages

--- modules/hosts/all-mgmt/librenms.nix
+++ modules/hosts/all-mgmt/librenms.nix
@@ -1,23 +1,27 @@
 {
-  flake.modules.nixos."hosts/all-mgmt" = {
-    services.librenms = {
-      enable = true;
-      hostname = "all-mgmt.verdienstnix.bundesheer.bigtopo";
-      database = {
-        createLocally = true;
-        socket = "/run/mysqld/mysqld.sock";
+  flake.modules.nixos."hosts/all-mgmt" =
+    { pkgs, ... }:
+    {
+      services.librenms = {
+        enable = true;
+        hostname = "all-mgmt.verdienstnix.bundesheer.bigtopo";
+        database = {
+          createLocally = true;
+          socket = "/run/mysqld/mysqld.sock";
+        };
+        settings = {
+          "nets" = [ "10.1.0.0/24" ];
+          "discovery_by_ip" = true;
+          "snmp.community" = [ "public" ];
+          "discovery_modules.discovery-arp" = true;
+        };
       };
-      settings = {
-        "nets" = [ "10.1.0.0/24" ];
-        "discovery_by_ip" = true;
-        "snmp.community" = [ "public" ];
-        "discovery_modules.discovery-arp" = true;
-      };
-    };
 
-    networking.firewall.allowedTCPPorts = [
-      80
-      443
-    ];
-  };
+      environment.systemPackages = [ pkgs.php ];
+
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+    };
 }
