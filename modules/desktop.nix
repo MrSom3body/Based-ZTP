ad6595a377668565d7c4a60fb0954e8df53387f9
Karun Sandhu <karun@sndh.dev>
2026-04-13 23:32:25 +0200

    desktop: set up mail

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,6 +3,30 @@
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
 
-    programs.thunderbird.enable = true;
+    programs.firefox.enable = true;
+
+    programs.thunderbird = {
+      enable = true;
+      policies = {
+        ImproveSearchSuggestionsDefault = false;
+        DisableAppUpdate = true;
+        AccountProvisioner = false;
+        "3rdparty".Extensions."thunderbird@example.com" = { };
+        EmailPreferences = {
+          emailAddress = "";
+          emailIncomingServer = {
+            secure = true;
+            hostname = "mail.verdienstnix.bundesheer.bigtopo";
+            port = 993;
+            type = "imap";
+          };
+          emailOutgoingServer = {
+            secure = true;
+            hostname = "mail.verdienstnix.bundesheer.bigtopo";
+            port = 587;
+          };
+        };
+      };
+    };
   };
 }
