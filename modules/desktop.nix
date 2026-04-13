5467975d9867d860d3e375f51626f1db4508a5c7
Karun Sandhu <karun@sndh.dev>
2026-04-13 13:37:23 +0200

    desktop: fix

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,9 +1,8 @@
-{ pkgs, ... }:
 {
   flake.modules.nixos.desktop = {
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
 
-    environment.systemPackages = [ pkgs.thunderbird ];
+    programs.thunderbird.enable = true;
   };
 }
