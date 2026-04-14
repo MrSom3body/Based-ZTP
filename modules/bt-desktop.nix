9e30a8c96c9af8abb6bdf971695845174b310467
Karun Sandhu <karun@sndh.dev>
2026-04-14 09:37:32 +0200

    bt-desktop: add certificate

--- modules/bt-desktop.nix
+++ modules/bt-desktop.nix
@@ -1,32 +1,38 @@
 {
-  flake.modules.nixos.bt-desktop = {
-    services.displayManager.gdm.enable = true;
-    services.desktopManager.gnome.enable = true;
+  flake.modules.nixos.bt-desktop =
+    { lib, ... }:
+    {
+      services.displayManager.gdm.enable = true;
+      services.desktopManager.gnome.enable = true;
 
-    programs.firefox.enable = true;
+      programs.firefox.enable = true;
 
-    programs.thunderbird = {
-      enable = true;
-      policies = {
-        ImproveSearchSuggestionsDefault = false;
-        DisableAppUpdate = true;
-        AccountProvisioner = false;
-        "3rdparty".Extensions."thunderbird@example.com" = { };
-        EmailPreferences = {
-          emailAddress = "";
-          emailIncomingServer = {
-            secure = true;
-            hostname = "mail.verdienstnix.bundesheer.bigtopo";
-            port = 993;
-            type = "imap";
-          };
-          emailOutgoingServer = {
-            secure = true;
-            hostname = "mail.verdienstnix.bundesheer.bigtopo";
-            port = 587;
+      programs.thunderbird = {
+        enable = true;
+        policies = {
+          ImproveSearchSuggestionsDefault = false;
+          DisableAppUpdate = true;
+          AccountProvisioner = false;
+          # Install internal CA so Thunderbird trusts the mail server cert
+          Certificates.Install = lib.optional (builtins.pathExists ./pki/ca.pem) (
+            builtins.toString ./pki/ca.pem
+          );
+          "3rdparty".Extensions."thunderbird@example.com" = { };
+          EmailPreferences = {
+            emailAddress = "";
+            emailIncomingServer = {
+              secure = true;
+              hostname = "mail.verdienstnix.bundesheer.bigtopo";
+              port = 993;
+              type = "imap";
+            };
+            emailOutgoingServer = {
+              secure = true;
+              hostname = "mail.verdienstnix.bundesheer.bigtopo";
+              port = 587;
+            };
           };
         };
       };
     };
-  };
 }
