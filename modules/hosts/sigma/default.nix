cb4c548cd7d1bb3996c49ce9deb9fb33a0099696
Karun Sandhu <karun@sndh.dev>
2026-04-16 01:31:12 +0200

    hosts/sigma: fix IPs

--- modules/hosts/sigma/default.nix
+++ modules/hosts/sigma/default.nix
@@ -3,11 +3,15 @@
   flake.modules.nixos."hosts/sigma" = {
     networking = {
       defaultGateway = {
-        interface = "ens33";
+        interface = "ens160";
         address = "10.0.1.254";
       };
       interfaces = {
-        "ens33".ipv4.addresses = lib.singleton {
+        "ens160".ipv4.addresses = lib.singleton {
+          address = "10.40.21.61";
+          prefixLength = 24;
+        };
+        "ens192".ipv4.addresses = lib.singleton {
           address = "10.0.1.10";
           prefixLength = 24;
         };
