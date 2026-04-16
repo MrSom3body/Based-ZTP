5092bdf7a249475e12768bb9e9737cbf776d879f
Karun Sandhu <karun@sndh.dev>
2026-04-16 09:17:44 +0200

    checkmk-server: setup caddy in front as a reverse proxy

--- modules/checkmk-server/default.nix
+++ modules/checkmk-server/default.nix
@@ -2,8 +2,15 @@
   flake.modules.nixos.checkmk-server = {
     imports = [ ./_compose.nix ];
     networking.firewall.allowedTCPPorts = [
-      8080
+      80
       8000
     ];
+
+    services.caddy = {
+      enable = true;
+      virtualHosts."http://monitoring.infra.nix".extraConfig = ''
+        reverse_proxy localhost:8080
+      '';
+    };
   };
 }
